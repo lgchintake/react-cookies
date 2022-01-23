13a75cbf6a310bd7941eb136e53865eb9461cf47
lgchintake <laxminarayan.lexicon@gmail.com>
2022-01-23 12:29:18 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const jwt = require('express-jwt');
+const jsonwebtoken = require('jsonwebtoken');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const app = express();
+app.use(cors());
+app.use(cookieParser());
+const jwtSecret = 'secret123';
+app.get('/jwt', (req, res) => {
+    const token = jsonwebtoken.sign({ user: 'johndoe' }, jwtSecret);
+    res.cookie('token', token, { httpOnly: true });
+    res.json({ token });
+});
+
+// app.use(
+//     jwt({
+//       secret: 'secret123',
+//       algorithms: ['HS256'],
+//       getToken: req => req.cookies.token
+//     })
+//   );
+const foods = [
+    { id: 1, description: 'burritos' },
+    { id: 2, description: 'quesadillas' },
+    { id: 3, description: 'churos' }
+];
+app.get('/foods', (req, res) => {
+    console.log(req.cookies)
+    res.json(foods);
+});
+app.listen(3001);
+console.log('App running on localhost:3001');
